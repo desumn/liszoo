1dd0995922ef282ccbc3ff204e6453cce8585143
desumn <ilyess.tahar@posteo.net>
2026-05-12 14:53:13 +0200

    add term module

---
+++ lib/common/common.ml
@@ -0,0 +1,3 @@
+module StringSet = Set.Make(String)
+
+
---
+++ lib/formula/term.ml
@@ -0,0 +1,60 @@
+open StdLabels
+
+type t =
+  | Var of string
+  | Const of int
+  | Neg of t
+  | Add of t * t
+  | Sub of t * t
+  | Mul of t * t
+
+let rec free_vars = function
+  | Var var -> Common.StringSet.singleton var
+  | Const _ -> Common.StringSet.empty
+  | Neg term -> free_vars term
+  | Add (l, r)
+  | Sub (l, r)
+  | Mul (l, r) -> Common.StringSet.union (free_vars l) (free_vars r)
+
+let rec subst var ~term ~subst_term =
+  match term with
+  | Var target when var = target -> subst_term
+  | Var target -> Var target
+  | Const c -> Const c
+  | Neg term -> subst var ~term ~subst_term
+  | Add (l, r) -> Add(subst var ~term:l ~subst_term, subst var ~term:r ~subst_term)
+  | Sub (l, r) -> Sub(subst var ~term:l ~subst_term, subst var ~term:r ~subst_term)
+  | Mul (l, r) -> Mul(subst var ~term:l ~subst_term, subst var ~term:r ~subst_term)
+
+
+let add_level = 1
+let sub_level = 1
+let mul_level = 2
+let neg_level = 3
+
+let paren_if cond ppf do_ =
+  if cond then (Fmt.pf ppf "("; do_ (); Fmt.pf ppf ")")
+  else do_ ()
+
+let rec pp_at level ppf term = 
+  match term with
+  | Var var -> Fmt.string ppf var
+  | Const const -> Fmt.int ppf const
+  | Neg term ->
+    paren_if (level > neg_level) ppf @@ fun () ->
+      Fmt.pf ppf "-%a" (pp_at (neg_level + 1)) term 
+  | Add (l, r) -> 
+    paren_if (level > add_level) ppf @@ fun () ->
+      Fmt.pf ppf "@[<2>%a +@ %a@]" (pp_at add_level) l (pp_at (add_level + 1)) r
+  | Sub (l, r) -> 
+    paren_if (level > sub_level) ppf @@ fun () ->
+      Fmt.pf ppf "@[<2>%a -@ %a@]" (pp_at sub_level) l (pp_at (sub_level + 1)) r
+  | Mul (l, r) -> 
+    paren_if (level > mul_level) ppf @@ fun () ->
+      Fmt.pf ppf "@[<2>%a *@ %a@]" (pp_at mul_level) l (pp_at (mul_level + 1)) r
+
+let pp = pp_at 0
+
+let equal (term1 : t) (term2 : t) = term1 = term2
+
+let compare (term1 : t) (term2 : t) = compare term1 term2
