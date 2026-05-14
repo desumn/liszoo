9d02464b3a2a7b5e5999d8de0ee3b472d4ccf3ea
desumn <ilyess.tahar@posteo.net>
2026-05-14 19:41:04 +0200

    move atom and term modules into formula file, to prepare for ite

--- lib/formula/formula.ml
+++ lib/formula/formula.ml
@@ -1,15 +1,142 @@
-module Term = Term
-module Atom = Atom
-
 type t =
   | Top
   | Bot
-  | Atom of Atom.t
+  | Atom of atom
   | Not of t
   | And of t * t
   | Or of t * t
   | Imp of t * t
   | Iff of t * t
+
+and term =
+  | Var of string
+  | Const of int
+  | Neg of term
+  | Add of term * term
+  | Sub of term * term
+  | Mul of term * term
+
+and atom =
+  | Eq of term * term
+  | Neq of term * term
+  | Lt of term * term
+  | Le of term * term
+  | Gt of term * term
+  | Ge of term * term
+
+module Term = struct
+  type t = term =
+    | Var of string
+    | Const of int
+    | Neg of t
+    | Add of t * t
+    | Sub of t * t
+    | Mul of t * t
+
+  let rec free_vars = function
+    | Var var -> Common.StringSet.singleton var
+    | Const _ -> Common.StringSet.empty
+    | Neg term -> free_vars term
+    | Add (l, r) | Sub (l, r) | Mul (l, r) ->
+        Common.StringSet.union (free_vars l) (free_vars r)
+
+  let rec subst var ~by ~on =
+    match on with
+    | Var target when var = target -> by
+    | Var target -> Var target
+    | Const c -> Const c
+    | Neg t -> Neg (subst var ~on:t ~by)
+    | Add (l, r) -> Add (subst var ~on:l ~by, subst var ~on:r ~by)
+    | Sub (l, r) -> Sub (subst var ~on:l ~by, subst var ~on:r ~by)
+    | Mul (l, r) -> Mul (subst var ~on:l ~by, subst var ~on:r ~by)
+
+  let add_level = 1
+  let sub_level = 1
+  let mul_level = 2
+  let neg_level = 3
+
+  let rec pp_at level ppf term =
+    let open Common.Pretty in
+    match term with
+    | Var var -> Fmt.string ppf var
+    | Const const -> Fmt.int ppf const
+    | Neg term ->
+        paren_if (level > neg_level) ppf @@ fun () ->
+        Fmt.pf ppf "-%a" (pp_at (neg_level + 1)) term
+    | Add (l, r) ->
+        paren_if (level > add_level) ppf @@ fun () ->
+        Fmt.pf ppf "@[<2>%a +@ %a@]" (pp_at add_level) l
+          (pp_at (add_level + 1))
+          r
+    | Sub (l, r) ->
+        paren_if (level > sub_level) ppf @@ fun () ->
+        Fmt.pf ppf "@[<2>%a -@ %a@]" (pp_at sub_level) l
+          (pp_at (sub_level + 1))
+          r
+    | Mul (l, r) ->
+        paren_if (level > mul_level) ppf @@ fun () ->
+        Fmt.pf ppf "@[<2>%a *@ %a@]" (pp_at mul_level) l
+          (pp_at (mul_level + 1))
+          r
+
+  let pp = pp_at 0
+  let equal (term1 : t) (term2 : t) = term1 = term2
+  let compare term1 term2 = compare term1 term2
+end
+
+module Atom = struct
+  type t = atom =
+    | Eq of Term.t * Term.t
+    | Neq of Term.t * Term.t
+    | Lt of Term.t * Term.t
+    | Le of Term.t * Term.t
+    | Gt of Term.t * Term.t
+    | Ge of Term.t * Term.t
+
+  let free_vars atom =
+    match atom with
+    | Eq (l, r) | Neq (l, r) | Lt (l, r) | Le (l, r) | Gt (l, r) | Ge (l, r) ->
+        Common.StringSet.union (Term.free_vars l) (Term.free_vars r)
+
+  let subst name subst_term atom =
+    match atom with
+    | Eq (l, r) ->
+        Eq
+          ( Term.subst name ~by:subst_term ~on:l,
+            Term.subst name ~by:subst_term ~on:r )
+    | Neq (l, r) ->
+        Neq
+          ( Term.subst name ~by:subst_term ~on:l,
+            Term.subst name ~by:subst_term ~on:r )
+    | Lt (l, r) ->
+        Lt
+          ( Term.subst name ~by:subst_term ~on:l,
+            Term.subst name ~by:subst_term ~on:r )
+    | Le (l, r) ->
+        Le
+          ( Term.subst name ~by:subst_term ~on:l,
+            Term.subst name ~by:subst_term ~on:r )
+    | Gt (l, r) ->
+        Gt
+          ( Term.subst name ~by:subst_term ~on:l,
+            Term.subst name ~by:subst_term ~on:r )
+    | Ge (l, r) ->
+        Ge
+          ( Term.subst name ~by:subst_term ~on:l,
+            Term.subst name ~by:subst_term ~on:r )
+
+  let pp ppf atom =
+    match atom with
+    | Eq (l, r) -> Fmt.pf ppf "%a = %a" Term.pp l Term.pp r
+    | Neq (l, r) -> Fmt.pf ppf "%a <> %a" Term.pp l Term.pp r
+    | Lt (l, r) -> Fmt.pf ppf "%a < %a" Term.pp l Term.pp r
+    | Le (l, r) -> Fmt.pf ppf "%a <= %a" Term.pp l Term.pp r
+    | Gt (l, r) -> Fmt.pf ppf "%a > %a" Term.pp l Term.pp r
+    | Ge (l, r) -> Fmt.pf ppf "%a >= %a" Term.pp l Term.pp r
+
+  let equal (atom1 : t) (atom2 : t) = atom1 = atom2
+  let compare atom1 atom2 = compare atom1 atom2
+end
 
 let rec free_vars formula =
   match formula with
--- lib/formula/formula.mli
+++ lib/formula/formula.mli
@@ -1,15 +1,60 @@
-module Term = Term
-module Atom = Atom
-
 type t =
   | Top
   | Bot
-  | Atom of Atom.t
+  | Atom of atom
   | Not of t
   | And of t * t
   | Or of t * t
   | Imp of t * t
   | Iff of t * t
+
+and term = private
+  | Var of string
+  | Const of int
+  | Neg of term
+  | Add of term * term
+  | Sub of term * term
+  | Mul of term * term
+
+and atom = private
+  | Eq of term * term
+  | Neq of term * term
+  | Lt of term * term
+  | Le of term * term
+  | Gt of term * term
+  | Ge of term * term
+
+module Term : sig
+  type t = term =
+    | Var of string
+    | Const of int
+    | Neg of t
+    | Add of t * t
+    | Sub of t * t
+    | Mul of t * t
+
+  val free_vars : t -> Common.StringSet.t
+  val subst : string -> by:t -> on:t -> t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val pp : t Fmt.t
+end
+
+module Atom : sig
+  type t = atom =
+    | Eq of Term.t * Term.t
+    | Neq of Term.t * Term.t
+    | Lt of Term.t * Term.t
+    | Le of Term.t * Term.t
+    | Gt of Term.t * Term.t
+    | Ge of Term.t * Term.t
+
+  val free_vars : t -> Common.StringSet.t
+  val subst : string -> Term.t -> t -> t
+  val pp : t Fmt.t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+end
 
 val free_vars : t -> Common.StringSet.t
 val subst : string -> Term.t -> t -> t
