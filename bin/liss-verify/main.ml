54a5b1ce16ca15038e77fd7cda21a40b909b724d
desumn <ilyess.tahar@posteo.net>
2026-05-14 13:55:34 +0200

    use nottui for ui

--- bin/liss-verify/main.ml
+++ bin/liss-verify/main.ml
@@ -46,43 +46,57 @@ let get_line_num (vc : Wp.vc) =
   let start, _ = vc.loc in
   start.pos_lnum
 
-let check_and_output program show_goals =
+let check program =
   let vcs = Wp.verify program in
   let verdicts =
     List.map vcs ~f:(fun (vc : Wp.vc) ->
         (vc, Smt.Solver.check_alt_ergo vc.goal))
   in
-  let num_verdicts = List.length verdicts in
-  let succeeded = ref 0 in
-  let failed = ref 0 in
-  List.iteri verdicts ~f:(fun i ((vc : Wp.vc), result) ->
-      Fmt.pf Format.std_formatter "[%d/%d] %a (on line %d): " (i + 1)
-        num_verdicts Wp.pp_kind vc.kind (get_line_num vc);
-      match (result : (Smt.Solver.smt_result, [ `Msg of string ]) result) with
-      | Ok Valid ->
-          incr succeeded;
-          Fmt.string Format.std_formatter "valid\n";
-          if show_goals then
-            Fmt.pf Format.std_formatter "Goal: %a\n" Formula.pp vc.goal
-      | Ok Invalid ->
-          incr failed;
-          Fmt.string Format.std_formatter "invalid\n";
-          Fmt.pf Format.std_formatter "Goal: %a\n" Formula.pp vc.goal
-      | Ok (Unknown _) ->
-          incr failed;
-          Fmt.string Format.std_formatter "unknown\n";
-          Fmt.pf Format.std_formatter "Goal: %a\n" Formula.pp vc.goal
-      | Error (`Msg error_message) ->
-          incr failed;
-          Fmt.string Format.std_formatter "\n";
-          Fmt.pf Format.err_formatter "\nError: %s\n" error_message);
-  Fmt.pf Format.std_formatter "%d total, %d succeeded, %d failed\n" num_verdicts
-    !succeeded !failed
+  verdicts
 
-let parse_and_verify file show_goals =
+let verdict_line ~num ~total (vc : Wp.vc) (verdict : Smt.Solver.smt_result) =
+  let open Notty in
+  let open Nottui in
+  let icon, colour = match verdict with
+    | Valid   -> "✓", A.green
+    | Invalid -> "✗", A.red
+    | Unknown _ -> "?", A.yellow
+  in
+  let loc = Fmt.str "at line %d" (get_line_num vc) in
+  Ui.hcat [
+    Ui.atom (I.string A.(fg colour ++ st bold) icon);
+    Ui.space 2 1;
+    Ui.atom (I.strf "[%d/%d]" num total);
+    Ui.space 2 1;
+    Ui.resize ~w:30 (Nottui.Ui.atom (I.strf "%a" Wp.pp_kind vc.kind));
+    Ui.resize ~w:20 (Nottui.Ui.atom (I.string A.empty loc));
+    ]
+
+
+let parse_and_verify file =
   match parse_file file with
   | Ok (program, _content) ->
-    check_and_output program show_goals;
+    let open Nottui in 
+    let open Notty in
+    let verdicts = check program in
+    let num_verdicts = List.length verdicts in
+    let verdict_uis = List.mapi verdicts
+      ~f:(fun i (vc, verdict) ->
+        match verdict with
+        | Ok verdict -> verdict_line ~num:(i + 1) ~total:num_verdicts vc verdict
+        | Error (`Msg message)->
+          Ui.atom (Notty.I.string A.(fg lightred ++ st italic) message)
+      ) in
+    let ui = Ui.vcat verdict_uis in
+    let cols =
+      match Notty_unix.winsize Unix.stdout with
+      | Some (c, _) -> c
+      | None -> 80
+    in
+    let rows = Nottui.Ui.layout_height ui in
+    let renderer = Renderer.make () in
+    Renderer.update renderer (cols, rows) ui;
+    Notty_unix.output_image (Renderer.image renderer);
     Cmd.Exit.ok
   | Error (`Syntax_error diagnostic) -> 
     let diagnotic_pp = Grace_ansi_renderer.pp_diagnostic
@@ -98,7 +112,7 @@ let program_arg =
   let doc = "path to a liss program" in
   Arg.(required & pos 0 (some fpath) None & info [] ~docv:"FILE" ~doc)
 
-let show_goals_arg =
+let _show_goals_arg =
   let doc = "show all goals" in
   Arg.(value & flag & info [ "g"; "goals" ] ~doc)
 
@@ -106,6 +120,6 @@ let cmd =
   let doc = "Verify a liss program" in
   let info = Cmd.info "liss-verify" ~version:"0.1.0" ~doc in
   Cmd.v info
-    Term.(const parse_and_verify $ program_arg $ show_goals_arg)
+    Term.(const parse_and_verify $ program_arg)
 
 let () = exit (Cmd.eval' cmd)
