01a791a0756606f340342a68e7a2af43a13cc19c
desumn <ilyess.tahar@posteo.net>
2026-05-14 10:54:12 +0200

    start to move to incremental merlin api for better messages

--- lib/liss/liss.ml
+++ lib/liss/liss.ml
@@ -4,5 +4,33 @@ module Cmd = Cmd
 module Program = Program
 module Conv = Conv
 
-let parse_lexbuf lb = Parser.program Lexer.token lb
-let parse_string s = parse_lexbuf (Lexing.from_string s)
+exception Parse_error of string * (Lexing.position * Lexing.position)
+
+module I = Parser.MenhirInterpreter
+
+let succeed (ast : Program.t) = ast
+
+let fail (lexbuf : Lexing.lexbuf) (checkpoint : Program.t I.checkpoint) =
+  match checkpoint with
+  | I.HandlingError env ->
+    let _state_num = I.current_state_number env in
+    let msg =
+      match (* Parser_messages.message state_num *) "syntax error" with
+      | msg -> msg
+      | exception Not_found -> "syntaxe error (no message found)" 
+    in
+    let pos = lexbuf.lex_start_p, lexbuf.lex_curr_p in
+    raise (Parse_error (msg, pos))
+  | _ -> assert false
+
+let supplier (lexbuf : Lexing.lexbuf) () =
+  let token = Lexer.token lexbuf in
+  (token, lexbuf.lex_start_p, lexbuf.lex_curr_p)
+
+let parse_lexbuf (lexbuf : Lexing.lexbuf) =
+  let checkpoint = Parser.Incremental.program lexbuf.lex_curr_p in 
+  I.loop_handle succeed (fail lexbuf) (supplier lexbuf) checkpoint
+
+let parse_string string =
+  let lexbuf = Lexing.from_string string in
+  parse_lexbuf lexbuf
